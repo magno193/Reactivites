cc15d3020b27a2814ac9f2d3ca8cc10042ddd101
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-09-19 17:58:20 -0300

    Criando DBContext e service

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,15 +6,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
     public class Startup
-        {
+    {
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,6 +27,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
             services.AddControllers();
         }
 
