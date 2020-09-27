570481256562b4a286b4cea63537f5be20117480
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-09-27 10:25:25 -0300

    Ingnore banco e formatação

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,45 +13,37 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
 
-namespace API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            });
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            // app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+namespace API {
+  public class Startup {
+    public Startup (IConfiguration configuration) {
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices (IServiceCollection services) {
+      services.AddDbContext<DataContext> (opt => {
+        opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
+      });
+      services.AddControllers ();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+      if (env.IsDevelopment ()) {
+        app.UseDeveloperExceptionPage ();
+      }
+
+      // app.UseHttpsRedirection();
+
+      app.UseRouting ();
+
+      app.UseAuthorization ();
+
+      app.UseEndpoints (endpoints => {
+        endpoints.MapControllers ();
+      });
+    }
+  }
 }
