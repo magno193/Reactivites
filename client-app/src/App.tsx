af87d4c9c2beabbf45d3ed27d3d943a2ff46de63
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-09-30 01:03:08 -0300

    Class component demo

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg'
 import './App.css';
-import { cars } from './Car';
-import CarItem from './CarItem';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <ul>
-        {cars.map(car => (
-          <CarItem car={car} ></CarItem>
-        ))}
-      </ul>
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      values: [{ id: 1, name: 'Value 101' }, { id: 2, name: 'Value 102' }]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            {this.state.values.map((value: any) => (
+              <li>{value.name}</li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
-
-
 export default App;
