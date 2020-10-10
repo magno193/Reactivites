0120540b949c7c756fddc21b7d4b56d15089f057
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-10-09 22:27:52 -0300

    Criando controller para Activities

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,41 +15,52 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
 
-namespace API {
-  public class Startup {
-    public Startup (IConfiguration configuration) {
+namespace API
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
       Configuration = configuration;
     }
 
     public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices (IServiceCollection services) {
-      services.AddDbContext<DataContext> (opt => {
-        opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<DataContext>(opt =>
+      {
+        opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
       });
-      services.AddCors(opt => {
-        opt.AddPolicy("CorsPolicy", policy => {
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
           policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
         });
       });
-      services.AddControllers ();
+      services.AddMediatR(typeof(List.Handler).Assembly);
+      services.AddControllers();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
-      if (env.IsDevelopment ()) {
-        app.UseDeveloperExceptionPage ();
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
       }
 
       // app.UseHttpsRedirection();
 
-      app.UseRouting ();
+      app.UseRouting();
 
-      app.UseAuthorization ();
+      app.UseAuthorization();
       app.UseCors("CorsPolicy");
-      app.UseEndpoints (endpoints => {
-        endpoints.MapControllers ();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
       });
     }
   }
