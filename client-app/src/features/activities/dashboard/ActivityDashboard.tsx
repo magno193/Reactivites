71257c5d42f5404d778f72f44360f5cb9a520fd2
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-10-16 17:51:28 -0300

    Passando dados de activity para o form.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -35,7 +35,9 @@ const ActivityDashboard: React.FC<IProps> = ({
             setSelectedActivity={setSelectedActivity}
           />
         )}
-        {editMode && <ActivityForm setEditMode={setEditMode} />}
+        {editMode && (
+          <ActivityForm setEditMode={setEditMode} activity={activity!} />
+        )}
       </Grid.Column>
     </Grid>
   );
