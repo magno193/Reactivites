d9ecd3b40fb3e4e92f8fc7b8be0c1ae65c265367
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-10-19 23:51:13 -0300

    Adicionando compomente de loading

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,7 @@ import { IActivity } from '../models/activity';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 const App: React.FC = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -11,6 +12,7 @@ const App: React.FC = () => {
     null,
   );
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleSelectActivity = (id: string) => {
     setSelectedActivity(activities.filter(activity => activity.id === id)[0]);
@@ -48,17 +50,21 @@ const App: React.FC = () => {
   };
 
   useEffect(() => {
-    agent.Activities.list().then(response => {
-      const activities: IActivity[] = [];
+    agent.Activities.list()
+      .then(response => {
+        const activities: IActivity[] = [];
 
-      response.forEach(activity => {
-        activity.date = activity.date.split('.')[0];
-        activities.push(activity);
-      });
+        response.forEach(activity => {
+          activity.date = activity.date.split('.')[0];
+          activities.push(activity);
+        });
 
-      setActivities(activities);
-    });
+        setActivities(activities);
+      })
+      .then(() => setLoading(false));
   }, []);
+
+  if (loading) return <LoadingComponent content="Loading activities..." />;
 
   return (
     <>
